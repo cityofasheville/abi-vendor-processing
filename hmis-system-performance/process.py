b084de89267f6f30c0fad4c03de8f803bbfa0980
Eric Jackson <ejackson@ashevillenc.gov>
2021-08-23 20:46:37 -0400

    Pulling out as routine

--- hmis-system-performance/process.py
+++ hmis-system-performance/process.py
@@ -4,41 +4,41 @@ import json
 import sys
 import os
 
-def processM2(asset, prefix):
-  skipRows = asset["skipRows"]
-  skipCols = asset["skipCols"]
-  rowCount = asset["rowCount"]
-  print('Prefix = ' + prefix)
-  print('Processing ' + json.dumps(asset,indent=1))
-  path = prefix+'/'+asset['filename']
-  print('Path: ' + path)
-  book = xlrd.open_workbook(path, logfile=(open(os.devnull, 'w')))
-  sh = book.sheet_by_index(0)
-
-  fmt = 'Reading worksheet {0} with {1} rows, {2} columns'
-  print(fmt.format(sh.name, sh.nrows, sh.ncols))
+def extractXlsData(sheet, skipRows, skipCols, rowCount):
   rows = []
-  for rx in range(sh.nrows):
+  for rx in range(sheet.nrows):
     if rx < skipRows:
       continue
     if rx > skipRows + rowCount:
       break
     cols = []
-    for i in range(sh.ncols):
+    for i in range(sheet.ncols):
       if i < skipCols:
         continue
-      cols.append(sh.cell_value(rowx=rx, colx=i))
+      cols.append(sheet.cell_value(rowx=rx, colx=i))
     rows.append(cols)
-
   return rows
+
+def processM2(asset, prefix):
+  skipRows = asset["skipRows"]
+  skipCols = asset["skipCols"]
+  rowCount = asset["rowCount"]
+  sheetName = asset["sheetName"]
+  path = prefix+asset['filename']
+  book = xlrd.open_workbook(path, logfile=(open(os.devnull, 'w')))
+  sh = book.sheet_by_name(sheetName)
+
+  fmt = 'Reading worksheet {0} with {1} rows, {2} columns'
+  print(fmt.format(sh.name, sh.nrows, sh.ncols))
+  return extractXlsData(sh, skipRows, skipCols, rowCount)
 
 inputs = None
 with open('inputs.json', 'r') as inputsFile:
   inputs = json.load(inputsFile)
-prefix = './' + inputs['prefix']
+prefix = './' + inputs['prefix'] + '/'
 for nm in inputs['files']:
   asset = inputs['files'][nm]
-  outputFileName = prefix + '/'+nm+'.csv'
+  outputFileName = prefix + nm + '.csv'
   data = None
   if asset['active']:
     print('Process ' + nm)
