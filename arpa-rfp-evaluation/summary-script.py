34ad636c452ca212f3317f56265f478b65e505fe
cameronhenshaw <chenshaw@ashevillenc.gov>
2021-11-23 15:59:31 -0500

    Updated scripts and inputs"

--- arpa-rfp-evaluation/summary-script.py
+++ arpa-rfp-evaluation/summary-script.py
@@ -4,6 +4,8 @@ import json
 from csv import reader
 from google.oauth2 import service_account
 import pandas as pd
+from os.path import exists
+
 
 
 SERVICE_ACCOUNT_FILE = 'arpa-processing-202b3d5190f8.json'
@@ -13,19 +15,10 @@ creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 
-# The ID and range of a sample spreadsheet.
-# Links to spreadsheets generated for each of the evaluators
-EVALUATOR_LINKS_SPREADSHEET_ID = '1tAJXTaol2iNrT-ieLYyFPeaX5sUZDRoN3tz6IGR3XNc'
-# Master Output Spreadsheet
-WRITE_SPREADSHEET_ID = '1nFyGhmkFGMdq1HzzTIp6aRtTpfIxgWeKN-veUfef_Eg'
-# Old evaluation sheet with scoring info on it (Master scoring Rubric sheet)
-SCORE_SPREADSHEET_ID = '1-AemNS14zBpFWeWCKqqhnbk_34FCEKTh3IfncyIYUxU'
-# Spreadsheet with just a list of links to project proposals (Assignment sheet)
-PROJECT_LINKS_SPREADSHEET_ID = '1oy7i8HOhDbxsvsXcwRnrBUV75o3giiS1tWeJQEhP-is'
-# Spreadsheet with links to individual tabs
-EACH_TAB_SPREADSHEET_ID = '1FssUUapqP8cgZBU0t2hMOdHmagCqRBksB-dhOWjQt2k'
+INPUTS_EVAL_MAPPING_ID =None
+OUTPUTS_MASTER_ID = None
+INPUTS_SPREADSHEET_ID = None
 
-# Do you think we have enough spreadsheets yet
 
 service = build('sheets', 'v4', credentials=creds)
 drive_service = build('drive', 'v3', credentials=creds)
@@ -41,11 +34,11 @@ sheet = service.spreadsheets()
 # A list with that info is passed to the third function, which creates the summary list using
 # all of the evaluator's scores for each project.
 
-def grab_weights_and_links(link_sheet_id, weight_sheet_id):
+def grab_weights_and_links(INPUTS_SPREADSHEET_ID):
     # Gets score weights from the evaluation sheet, and project links, and puts these things into 2
     # dfs to merge with the main summary df later
     sheet = service.spreadsheets()
-    results = sheet.values().get(spreadsheetId=weight_sheet_id,range='Score Weighting!C8:D27').execute()
+    results = sheet.values().get(spreadsheetId=INPUTS_SPREADSHEET_ID,range='Score Weighting!C8:D27').execute()
     values = results.get('values', [])
     del values[13]
     del values[6]
@@ -56,18 +49,20 @@ def grab_weights_and_links(link_sheet_id, weight_sheet_id):
 
     # Gets project links from the evaluation assignment sheet
     sheet = service.spreadsheets()
-    results = sheet.values().get(spreadsheetId=link_sheet_id,range='Eligible Proposals and Assignments!A2:C').execute()
+    results = sheet.values().get(spreadsheetId=INPUTS_SPREADSHEET_ID,range='Eligible Proposals and Assignments!A2:C').execute()
     values = results.get('values', [])
-    link_df = pd.DataFrame(values, columns=['project_number', 'project_name', 'project_link'])
-    return(link_df, weight_df)
+    links_df = pd.DataFrame(values, columns=['project_number', 'project_name', 'project_link'])
+
+
+    return(links_df, weight_df)
 
 
 
 
-def build_project_summary_list(weight_df, EVALUATOR_LINKS_SPREADSHEET_ID, EACH_TAB_SPREADSHEET_ID):
+def build_project_summary_list(links_df, weight_df, INPUTS_EVAL_MAPPING_ID):
     # Creating a dataframe with links to individual tabs to use later
     sheet = service.spreadsheets()
-    results = sheet.values().get(spreadsheetId=EACH_TAB_SPREADSHEET_ID,range='Sheet1!A1:C').execute()
+    results = sheet.values().get(spreadsheetId=INPUTS_EVAL_MAPPING_ID,range='Tab Mapping!A1:AB').execute()
     tabs = results.get('values', [])
     tab_links_df = pd.DataFrame(tabs)
     tab_links_df.iloc[0,0] = 'Project'
@@ -79,22 +74,16 @@ def build_project_summary_list(weight_df, EVALUATOR_LINKS_SPREADSHEET_ID, EACH_T
 
     # Get spreadsheet links/ids from the spreadsheet
     total_list = []
-    results = sheet.values().get(spreadsheetId=EVALUATOR_LINKS_SPREADSHEET_ID,range='Copy of Sheet1!A2:B').execute()
+    results = sheet.values().get(spreadsheetId=INPUTS_EVAL_MAPPING_ID,range='Sheet Mapping!A2:C').execute()
     link_ss_values = results.get('values', [])
 
     for thing in link_ss_values:
-        item = thing[1]
-
-        # Gets id and range of tabs for each sheet
-        item = item.split('/d/')
-        item = item[1].split('/edit')
-        id = item[0]
+        id = thing[1]
 
         sheet = service.spreadsheets()
         sheets = sheet.get(spreadsheetId=id, fields='sheets/properties/title').execute()
         ranges = [sheet['properties']['title'] for sheet in sheets['sheets']]
         
-
         format_list = []
 
         # Goes through each tab and gets values
@@ -130,27 +119,24 @@ def build_project_summary_list(weight_df, EVALUATOR_LINKS_SPREADSHEET_ID, EACH_T
             total_score = ECI_score + PPE_score + OQ_score 
 
             #Grabbing info from list to put into the right output format
-            project_name = values[0][1].split(": ",1)[1]
-            project_number = project_name[1]
-            evaluator = values[3][1].split(": ",1)[1]
+            project_name = values[1][1].split(": ",1)[1]
+            project_number = project_name[0]
+            evaluator = values[0][1].split(": ",1)[1]
             evaluator=evaluator.strip()
-            link = values[2][1]
+            link = thing[2]
 
             # Using the df from the beginning of this function to look up the links
             # to individual tabs on evaluator sheets. Appending that to the end of the list.
-
             eval_link = tab_links_df[evaluator].iloc[int(project_number)-1]
 
 
             format_list = [project_number, project_name, evaluator, link, total_score, ECI_score, PPE_score, OQ_score, eval_link]
             total_list.append(format_list)
-
     return(total_list)
 
 
 
-
-def summarize_all_project(my_list, link_df):
+def summarize_all_project(my_list, links_df):
     # Creating initial df
     my_df = pd.DataFrame(my_list, columns=['project_number', 'project_name', 'evaluator', 'link_to_proposal', 
                     'total_score', 'ECI_score', 'PPE_score', 'OQ_score', 'eval_link'])
@@ -158,7 +144,8 @@ def summarize_all_project(my_list, link_df):
 
 
     #Calculating mean and median, renaming columsn and resetting index (so that project #s show up when converted to list)
-    summary_df = pd.DataFrame(my_df.groupby(['project_number', 'project_name', 'link_to_proposal'])['total_score', 'ECI_score', 'PPE_score', 'OQ_score'].mean())
+    #summary_df = pd.DataFrame(my_df.groupby(['project_number', 'project_name', 'link_to_proposal'])['total_score', 'ECI_score', 'PPE_score', 'OQ_score'].mean())
+    summary_df = pd.DataFrame(my_df.groupby(['project_number', 'project_name'])['total_score', 'ECI_score', 'PPE_score', 'OQ_score'].mean())
     summary_df = summary_df.reset_index()
 
     median_df = pd.DataFrame(my_df.groupby(['project_name'])['total_score'].median())
@@ -174,17 +161,18 @@ def summarize_all_project(my_list, link_df):
   
 
     # Merging the various dfs to create 1 summary
+    #summary_df=summary_df.merge(links_df, on='project_name')
     summary_df=summary_df.merge(median_df, on='project_name')
     summary_df=summary_df.merge(individual_score_list_df, on='project_name')
-    summary_df = summary_df.merge(link_df[['project_number', 'project_link']], on='project_number', how='left')
+    summary_df = summary_df.merge(links_df[['project_number', 'project_link']], on='project_number', how='left')
     summary_df=summary_df.merge(eval_links_df, on='project_name')
 
 
 
     # Reordering columns so the info is in the correct order in the list 
     summary_df = summary_df[['project_number', 'project_name', 'project_link', 'median_score', 'total_score',
-       'ECI_score', 'PPE_score', 'OQ_score', 'score_list', 'link_to_proposal', 'eval_link']]
-    summary_df = summary_df.drop('link_to_proposal', 1)
+       'ECI_score', 'PPE_score', 'OQ_score', 'score_list', 'eval_link']]
+    #summary_df = summary_df.drop('link_to_proposal', 1)
     summary_df = summary_df.round(2)
 
     final_list = summary_df.values.tolist()
@@ -203,12 +191,25 @@ def summarize_all_project(my_list, link_df):
 
 ###########################################################################
 
-link_df, weight_df = grab_weights_and_links(PROJECT_LINKS_SPREADSHEET_ID, SCORE_SPREADSHEET_ID)
+inputs = None
+if exists('./inputs.json'):
+    with open('inputs.json', 'r') as file:
+        inputs = json.load(file)
+else:
+    print('You must create an inputs.json file')
+    sys.exit()
+
+
+INPUTS_EVAL_MAPPING_ID = inputs["INPUTS_EVAL_MAPPING_ID"]
+OUTPUTS_MASTER_ID = inputs["OUTPUTS_MASTER_ID"]
+INPUTS_SPREADSHEET_ID = inputs['INPUTS_SPREADSHEET_ID']
+
+links_df, weight_df = grab_weights_and_links(INPUTS_SPREADSHEET_ID)
 
 
 # Calls list building function
-all_project_scores = build_project_summary_list(weight_df, EVALUATOR_LINKS_SPREADSHEET_ID, EACH_TAB_SPREADSHEET_ID)
-list_to_append = summarize_all_project(all_project_scores, link_df)
+all_project_scores = build_project_summary_list(links_df, weight_df, INPUTS_EVAL_MAPPING_ID)
+list_to_append = summarize_all_project(all_project_scores, links_df)
 
 
 #Update Spreadsheet
@@ -218,12 +219,12 @@ resource = {
 }
 
 service.spreadsheets().values().update(
-  spreadsheetId=WRITE_SPREADSHEET_ID,
+  spreadsheetId=OUTPUTS_MASTER_ID,
   range="Summary!A2:AA1000",
   body=resource,
   valueInputOption="USER_ENTERED").execute()
 
-print('boop')
+print('Finished, Party time')
 
 
 #############################################
