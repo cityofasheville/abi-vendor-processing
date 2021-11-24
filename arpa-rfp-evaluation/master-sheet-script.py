fceed6f2f4e1a9a7068b04596753f32531a01de7
Eric Jackson <ejackson@ashevillenc.gov>
2021-11-24 18:27:57 -0500

    Some rewriting/bug fixing, added column for completeness

--- arpa-rfp-evaluation/master-sheet-script.py
+++ arpa-rfp-evaluation/master-sheet-script.py
@@ -1,5 +1,7 @@
 from googleapiclient.discovery import build
 import json
+import sys
+import time
 from csv import reader
 from google.oauth2 import service_account
 import pandas as pd
@@ -7,12 +9,8 @@ from os.path import exists
 
 
 
-SERVICE_ACCOUNT_FILE = 'arpa-processing-202b3d5190f8.json'
+SERVICE_ACCOUNT_FILE = None
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-
-creds = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
 
 # The ID and range of a sample spreadsheet.
 OUTPUTS_MASTER_ID = None
@@ -30,73 +28,72 @@ def setUpServices():
   sheetService = build('sheets', 'v4', credentials=creds)
   #driveService = build('drive', 'v3', credentials=creds)
 
+def stripLower(lst):
+    return list(map(lambda itm: itm.strip().lower() if itm else None, lst))
 
-def build_list(all_cat_list, INPUTS_EVAL_MAPPING_ID):
-    format_list = []
+def getSheetTitles(sheet, spreadsheetId):
+    sheets = sheet.get(spreadsheetId=spreadsheetId, fields='sheets/properties/title').execute()
+    return [sheet['properties']['title'] for sheet in sheets['sheets']]
+
+def build_list(allCategories, INPUTS_EVAL_MAPPING_ID):
+    allQuestions = []
     sheet = sheetService.spreadsheets()
 
-    # Reads spreadsheet to get list from the spreadsheet
+    # Read the mapping of evaluator to spreadsheet ID/URL. We just use the spreadsheet ID
     total_list = []
     results = sheet.values().get(spreadsheetId=INPUTS_EVAL_MAPPING_ID,range='Sheet Mapping!A2:C').execute()
-    link_ss_values = results.get('values', [])
+    evaluatorMap = results.get('values', [])
 
-    for entry in link_ss_values:
-        id = entry[1]
-        sheets = sheet.get(spreadsheetId=id, fields='sheets/properties/title').execute()
-        ranges = [sheet['properties']['title'] for sheet in sheets['sheets']]
-        
-        for tab in ranges[1:]:
-            results = sheet.values().get(spreadsheetId=id,range=tab +'!A1:R24').execute()
-            values = results.get('values', [])
+    # For each evaluator, iterate through each evaluation to get categories and answers
+    for evaluatorEntry in evaluatorMap:
+        evaluator = evaluatorEntry[0]
+        print('Reading evaluator ' + evaluator)
+        id = evaluatorEntry[1] # ID of this evaluator's spreadsheet
+        link = evaluatorEntry[2]
+        tabs = getSheetTitles(sheet, id)
+
+        for tab in tabs[1:]:
+            print(' Working on tab ' + tab)
+            # ? Why R24, not E24 ?
+            values = sheet.values().get(spreadsheetId=id,range=tab +'!A1:R24').execute().get('values', [])
+            projectName = values[1][1].split(": ",1)[1] 
+            projectNumber = projectName.split(' ')[0]
+            #Generates list of categories for the specific project
+            projectCategories = stripLower(values[2][1].split(": ")[1].split(', '))
+
+            # Set up category flags for this project
+            categoryFlags = ['no'] * len(allCategories)
+            for category in allCategories:
+                if category in projectCategories:
+                    categoryFlags[allCategories.index(category)] = 'yes'
+            countResponses = 0
+            # Need to delay appending to allQuestions until we know # responses 
+            holdQuestions = []
             # Goes through each row. For each, builds a list of the needed values
-            for number in range(6,24):
-                evaluator = values[0][1].split(": ",1)[1] 
-                q_number = values[number][0]
-                project_name = values[1][1].split(": ",1)[1] 
-                project_number = project_name[0]
-                link = entry[2]
-                question_cat = values[number][4]
-                response = values[number][2]
-                short_list = [evaluator, project_number, project_name, link, q_number, question_cat, response]
-                
-                # Adds "no" responses for the categories
-                for number in range(0,10):
-                    short_list.append('no')
-                
-                #Generates list of categories for the specific project
-                project_cat_fixed = []
-                project_cat= values[1][1].split(": ")[1]
-                project_cat_list = project_cat.split(', ')
-                for item in project_cat_list:
-                  j = item.strip().lower()
-                  project_cat_fixed.append(j)
-                project_cat_list = project_cat_fixed
-
-                # Goes through the the list of this project's categories to see if there are any matches with
-                # the project category columsn. If there is, the value for that column is replaced with
-                # 'yes' in the short list
-                for category in all_cat_list:
-                    if category in project_cat_list:
-                        index = all_cat_list.index(category)
-                        short_list[6 + index] = 'yes'
-
-                format_list.append(short_list)
-    return(format_list)
+            for row in range(6,24):
+                qNumber = values[row][0]
+                qCategory = values[row][4]
+                response = values[row][2]
+                if response:
+                    countResponses += 1
+                short_list = [evaluator, projectNumber, projectName, link, qNumber, qCategory, response, 'no']
+                short_list.extend(categoryFlags)
+                holdQuestions.append(short_list)
+            for row in holdQuestions:
+                if (countResponses == 18):
+                    row[7] = 'yes'
+                allQuestions.append(row)
+            time.sleep(1)
 
 
-def create_category_list(OUTPUTS_MASTER_ID):
-  results = sheet.values().get(spreadsheetId=OUTPUTS_MASTER_ID,range='All Data!A1:R1').execute()
-  values = results.get('values', [])
+        break
 
-  all_cat_list = values[0][7:17] #This range gets a list of categories from the spreadsheet
-  all_cat_list_fixed = []
+    return allQuestions
 
-  for item in all_cat_list:
-    j = item.strip().lower()
-    all_cat_list_fixed.append(j)
-
-  all_cat_list = all_cat_list_fixed
-  return(all_cat_list)
+# Read the list of categories, strip whitespace and lowercase them
+def readCategories(OUTPUTS_MASTER_ID):
+  values = sheetService.spreadsheets().values().get(spreadsheetId=OUTPUTS_MASTER_ID,range='All Data!A1:R1').execute().get('values', [])
+  return stripLower(values[0][8:18])
 
 ############################ Main Program Start
 
@@ -109,20 +106,18 @@ else:
     print('You must create an inputs.json file')
     sys.exit()
 
-# Set const values
+# Set global variables
 OUTPUTS_MASTER_ID = inputs["OUTPUTS_MASTER_ID"]
 INPUTS_EVAL_MAPPING_ID = inputs["INPUTS_EVAL_MAPPING_ID"]
+SERVICE_ACCOUNT_FILE = inputs["SERVICE_ACCOUNT_FILE"]
 
 setUpServices()
-sheet = sheetService.spreadsheets()
 
-# Gets the list of categories from the master sheet, which is passed
-# into the function
-
-all_cat_list = create_category_list(OUTPUTS_MASTER_ID)
+# Gets the list of categories from the master sheet
+allCategories = readCategories(OUTPUTS_MASTER_ID)
 
 # Calls list building function, creates the list to append to the spreadsheet
-list_to_append = build_list(all_cat_list, INPUTS_EVAL_MAPPING_ID)
+list_to_append = build_list(allCategories, INPUTS_EVAL_MAPPING_ID)
 
 
 # Update sheet
@@ -133,7 +128,7 @@ resource = {
 
 sheetService.spreadsheets().values().update(
   spreadsheetId=OUTPUTS_MASTER_ID,
-  range="All Data!A2:AA1000",
+  range="All Data!A2:AA10000",
   body=resource,
   valueInputOption="USER_ENTERED").execute()
 
